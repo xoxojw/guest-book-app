9703ec5b6c4f11474d32851d412aed8a008825b4
Jiwon Park <xoxojw.dev@gmail.com>
2023-08-04 11:44:21 +0900

    :sparkles: AuthLayout.tsx 완성

--- src/shared/AuthLayout.tsx
+++ src/shared/AuthLayout.tsx
@@ -12,9 +12,20 @@ const AuthLayout = () => {
 
     // TODO: localStorage의 토큰 검색
     // TODO: localStorage의 이메일 검색
+    const localToken = localStorage.getItem("token")
+    const localEmail = localStorage.getItem("email")
+    console.log(localToken, localEmail)
+
     // TODO: 토큰 또는 이메일 중 하나라도 없을 경우 "토큰 또는 이메일이 없습니다. 로그인해주세요." alert
     // TODO: localStorage에 있는 token, email을 제거
     // TODO: "/auth"로 이동
+    if (localToken && localEmail) return;
+    else {
+      alert("토큰 또는 이메일이 없습니다. 로그인해주세요.");
+      localStorage.removeItem("token");
+      localStorage.removeItem("email");
+      navigate("/auth");
+    }
   }, [navigate]);
 
   const handleLogoutButtonClick = () => {
@@ -24,6 +35,13 @@ const AuthLayout = () => {
     // TODO: yes 선택 시, localStorage의 token과 email 제거
     // TODO: "로그아웃이 완료되었습니다" alert
     // TODO: "/auth"로 이동
+    const confirmLogout = confirm("로그아웃 하시겠습니까?")
+    if (confirmLogout) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("email");
+      alert("로그아웃이 완료되었습니다.")
+      navigate("/auth")
+    }
   };
 
   return (
